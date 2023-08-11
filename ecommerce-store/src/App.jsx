cc41075bce9512b9f5792e5c352260356c263f2b
Deyvid_Ivanov <deivid.ivanov001@gmail.com>
2023-08-11 22:00:29 +0300

    add: filtering option

--- ecommerce-store/src/App.jsx
+++ ecommerce-store/src/App.jsx
@@ -4,12 +4,14 @@ import Header from './components/Header/Header.jsx';
 import ProductGrid from './components/ProductGrid/ProductGrid.jsx';
 import { getCartCount } from './services/shoppingCart.services.js';
 import ProductSorting from './components/ProductSorting/ProductSorting';
-import { Container } from '@mui/material';
+import ProductFiltering from './components/ProductFiltering/ProductFiltering'; // Import the new component
+import { Container, Grid } from '@mui/material';
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [cartCount, setCartCount] = useState(getCartCount());
   const [sortOption, setSortOption] = useState('alpha-asc'); // Default sorting option
+  const [filters, setFilters] = useState({}); // State to store filters
 
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
@@ -23,6 +25,10 @@ const App = () => {
     setSortOption(selectedSort);
   };
 
+  const handleFilterChange = (newFilters) => {
+    setFilters(newFilters);
+  };
+
   return (
     <div>
       <Header
@@ -31,14 +37,22 @@ const App = () => {
         cartCount={cartCount}
       />
       <Container>
-        <ProductSorting onSortChange={handleSortChange} />
-      <ProductGrid
-        selectedCategory={selectedCategory}
-        updateCartCount={handleCartCountUpdate}
-        sortOption={sortOption} // Pass the sorting option to ProductGrid
-      />
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            {/* Include the ProductFiltering component */}
+            <ProductFiltering onFilterChange={handleFilterChange} />
+          </Grid>
+          <Grid item xs={9}>
+            <ProductSorting onSortChange={handleSortChange} />
+            <ProductGrid
+              selectedCategory={selectedCategory}
+              updateCartCount={handleCartCountUpdate}
+              sortOption={sortOption}
+              filters={filters} // Pass the filters to ProductGrid
+            />
+          </Grid>
+        </Grid>
       </Container>
-      
     </div>
   );
 };
