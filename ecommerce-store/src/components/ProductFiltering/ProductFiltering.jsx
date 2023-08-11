710fabbbe6caa983b36bf20bb825b6a548e46947
Deyvid_Ivanov <deivid.ivanov001@gmail.com>
2023-08-11 22:01:22 +0300

    add: component for Filtering

---
+++ ecommerce-store/src/components/ProductFiltering/ProductFiltering.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Box, Checkbox, Slider, FormControlLabel, FormGroup, Typography, Button} from '@mui/material';
+import './ProductFiltering.css'
+
+const ProductFiltering = ({ onFilterChange }) => {
+  const [colorFilters, setColorFilters] = useState([]);
+  const [priceRange, setPriceRange] = useState([0, 1000]);
+
+  const handleColorChange = (event) => {
+    const selectedColor = event.target.name;
+    if (colorFilters.includes(selectedColor)) {
+      setColorFilters(colorFilters.filter(color => color !== selectedColor));
+    } else {
+      setColorFilters([...colorFilters, selectedColor]);
+    }
+  };
+
+  const handlePriceChange = (event, newValue) => {
+    setPriceRange(newValue);
+  };
+
+  const applyFilters = () => {
+    const filters = {
+      colors: colorFilters,
+      priceRange: priceRange,
+    };
+    onFilterChange(filters);
+  };
+
+  return (
+    <Box id="product-filtering" p={2}>
+      <Typography variant="h4">Filter By:</Typography>
+      <FormGroup>
+        {/* Color Filtering */}
+        <Typography variant='h6' className='filtering-category'>Color:</Typography>
+        <FormControlLabel
+          control={<Checkbox checked={colorFilters.includes('Languid Lavender')} onChange={handleColorChange} name="Languid Lavender" />}
+          label="Languid Lavender"
+        />
+        {/* Add more color checkboxes as needed */}
+        
+        {/* Price Range Filtering */}
+        <Typography variant='h6' className='filtering-category'>Price:</Typography>
+        <Slider
+          value={priceRange}
+          onChange={handlePriceChange}
+          valueLabelDisplay="auto"
+          min={0}
+          max={2500}
+        />
+        <Typography>Min: ${priceRange[0]} - Max: ${priceRange[1]}</Typography>
+        
+        {/* Apply Button */}
+        <Button variant="contained" color="primary" onClick={applyFilters} id='filtering-button'>
+          Apply Filters
+        </Button>
+      </FormGroup>
+    </Box>
+  );
+};
+
+ProductFiltering.propTypes = {
+  onFilterChange: PropTypes.func.isRequired,
+};
+
+export default ProductFiltering;
