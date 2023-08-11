dfd5444a70e6d47aca42c340515b9f4041259fef
Deyvid_Ivanov <deivid.ivanov001@gmail.com>
2023-08-11 15:22:55 +0300

    refactor: put the logic in App

--- ecommerce-store/src/App.jsx
+++ ecommerce-store/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import './App.css'
-import product from './data/product.json'
-import Header from './components/Header/Header'
-function App() {
-  const phones = product
+import React, { useState } from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Header from './components/Header/Header.jsx';
+import ProductGrid from './components/ProductGrid/ProductGrid.jsx';
+import { getCartCount,} from './services/shoppingCart.services.js';
+
+const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [cartCount, setCartCount] = useState(getCartCount());
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleCartCountUpdate = () => {
+    setCartCount(getCartCount()); // Update the cart count state
+  };
 
   return (
-    <>
-    <Header></Header>
-    </>
-  )
-}
+    <div>
+      <CssBaseline />
+      <Header
+        selectedCategory={selectedCategory}
+        handleCategoryClick={handleCategoryClick}
+        cartCount={cartCount}
+      />
+      <ProductGrid
+        selectedCategory={selectedCategory}
+        updateCartCount={handleCartCountUpdate}
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
