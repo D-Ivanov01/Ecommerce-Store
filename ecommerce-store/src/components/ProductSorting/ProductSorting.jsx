da8ad0e6191efd9e11936532540627ace767c8a9
Deyvid_Ivanov <deivid.ivanov001@gmail.com>
2023-08-11 20:06:23 +0300

    refactor: add responsive functionality

--- ecommerce-store/src/components/ProductSorting/ProductSorting.jsx
+++ ecommerce-store/src/components/ProductSorting/ProductSorting.jsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import "./ProductSorting.css";
+import React, { useState } from 'react';
+import './ProductSorting.css';
+import SideBar from '../SideBar/SideBar';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const ProductSorting = ({ onSortChange }) => {
+    const [showSidebar, setShowSidebar] = useState(false);
   
     const handleSortChange = (event) => {
-    const selectedSort = event.target.value;
-    onSortChange(selectedSort);
+      const selectedSort = event.target.value;
+      onSortChange(selectedSort);
+    };
+  
+    const toggleSidebar = () => {
+      setShowSidebar(!showSidebar);
+    };
+  
+    return (
+      <div className="product-sorting-container">
+        {/* Show sorting options when screen width is larger than 599px */}
+        <div className="desktop-sorting">
+          <label className="sort-label" htmlFor="sort-select">
+            Sort by:
+          </label>
+          <select className="sort-select" onChange={handleSortChange}>
+            <option value="alpha-asc">Alphabetical A-Z</option>
+            <option value="alpha-desc">Alphabetical Z-A</option>
+            <option value="price-asc">Price Ascending</option>
+            <option value="price-desc">Price Descending</option>
+          </select>
+        </div>
+  
+        {/* Show sidebar toggle button and icon when screen width is 599px or less */}
+        <div className="mobile-sorting">
+          <button className="sidebar-toggle-button" onClick={toggleSidebar}>
+            <KeyboardArrowRightIcon />
+          </button>
+          <SideBar open={showSidebar} onClose={toggleSidebar}>
+            <label className="sort-label" htmlFor="sort-select">
+              Sort by:
+            </label>
+            <select className="sort-select" onChange={handleSortChange}>
+              <option value="alpha-asc">Alphabetical A-Z</option>
+              <option value="alpha-desc">Alphabetical Z-A</option>
+              <option value="price-asc">Price Ascending</option>
+              <option value="price-desc">Price Descending</option>
+            </select>
+          </SideBar>
+        </div>
+      </div>
+    );
   };
-
-  return (
-    <div className="product-sorting-container">
-      <label className="sort-label" htmlFor="sort-select">
-        Sort by:
-      </label>
-      <select className="sort-select" onChange={handleSortChange}>
-        <option value="alpha-asc">Alphabetical A-Z</option>
-        <option value="alpha-desc">Alphabetical Z-A</option>
-        <option value="price-asc">Price Ascending</option>
-        <option value="price-desc">Price Descending</option>
-      </select>
-    </div>
-  );
-};
 
 export default ProductSorting;
