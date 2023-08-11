ff5beae0ed697f7c48810d24c609115055579faf
Deyvid_Ivanov <deivid.ivanov001@gmail.com>
2023-08-11 15:55:18 +0300

    refactor: handle the state of the sorting option

--- ecommerce-store/src/App.jsx
+++ ecommerce-store/src/App.jsx
@@ -2,31 +2,38 @@ import React, { useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Header from './components/Header/Header.jsx';
 import ProductGrid from './components/ProductGrid/ProductGrid.jsx';
-import { getCartCount,} from './services/shoppingCart.services.js';
+import { getCartCount } from './services/shoppingCart.services.js';
+import ProductSorting from './components/ProductSorting/ProductSorting';
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [cartCount, setCartCount] = useState(getCartCount());
+  const [sortOption, setSortOption] = useState('alpha-asc'); // Default sorting option
 
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
   };
 
   const handleCartCountUpdate = () => {
-    setCartCount(getCartCount()); // Update the cart count state
+    setCartCount(getCartCount());
+  };
+
+  const handleSortChange = (selectedSort) => {
+    setSortOption(selectedSort);
   };
 
   return (
     <div>
-      <CssBaseline />
       <Header
         selectedCategory={selectedCategory}
         handleCategoryClick={handleCategoryClick}
         cartCount={cartCount}
       />
+      <ProductSorting onSortChange={handleSortChange} />
       <ProductGrid
         selectedCategory={selectedCategory}
         updateCartCount={handleCartCountUpdate}
+        sortOption={sortOption} // Pass the sorting option to ProductGrid
       />
     </div>
   );
