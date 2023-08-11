f5efd5398f8bb9f74da6b9bbf98e99d86f283e4a
Deyvid_Ivanov <deivid.ivanov001@gmail.com>
2023-08-11 19:10:29 +0300

    refactor: put classes to labels

--- ecommerce-store/src/components/ProductSorting/ProductSorting.jsx
+++ ecommerce-store/src/components/ProductSorting/ProductSorting.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import "./ProductSorting.css";
 
 const ProductSorting = ({ onSortChange }) => {
-  const handleSortChange = (event) => {
+  
+    const handleSortChange = (event) => {
     const selectedSort = event.target.value;
     onSortChange(selectedSort);
   };
 
   return (
-    <div id="product-sorting">
-      <label htmlFor="sort-select">Sort by: </label>
-      <select id="sort-select" onChange={handleSortChange}>
+    <div className="product-sorting-container">
+      <label className="sort-label" htmlFor="sort-select">
+        Sort by:
+      </label>
+      <select className="sort-select" onChange={handleSortChange}>
         <option value="alpha-asc">Alphabetical A-Z</option>
         <option value="alpha-desc">Alphabetical Z-A</option>
         <option value="price-asc">Price Ascending</option>
