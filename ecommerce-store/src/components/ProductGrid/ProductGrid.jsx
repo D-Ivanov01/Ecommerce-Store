19437ddaddf7bd588bd55a3c3b59a9022f5f0b4c
Deyvid_Ivanov <deivid.ivanov001@gmail.com>
2023-08-11 15:54:40 +0300

    refactor: implement logic to ProductGrid

--- ecommerce-store/src/components/ProductGrid/ProductGrid.jsx
+++ ecommerce-store/src/components/ProductGrid/ProductGrid.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -10,7 +11,7 @@ import Alert from '@mui/material/Alert';
 import productsData from '../../data/product.json';
 import './ProductGrid.css';
 
-const ProductGrid = ({ selectedCategory, updateCartCount }) => {
+const ProductGrid = ({ selectedCategory, updateCartCount, sortOption }) => {
   const [showAlert, setShowAlert] = useState(false);
   const [numProductsToShow, setNumProductsToShow] = useState(20);
   const [numProductsToLoad, setNumProductsToLoad] = useState(20);
@@ -18,6 +19,20 @@ const ProductGrid = ({ selectedCategory, updateCartCount }) => {
   const filteredProducts = selectedCategory
     ? filterProductsByCategory(selectedCategory, productsData)
     : productsData.slice(0, numProductsToShow);
+  // Copy the filtered products array and then sort it based on the sortOption
+  const sortedProducts = [...filteredProducts];
+  if (sortOption === 'alpha-asc') {
+    sortedProducts.sort((a, b) => a.brand.localeCompare(b.brand));
+  } else if (sortOption === 'alpha-desc') {
+    sortedProducts.sort((a, b) => b.brand.localeCompare(a.brand));
+  } else if (sortOption === 'price-asc') {
+    sortedProducts.sort((a, b) => a.price - b.price);
+  } else if (sortOption === 'price-desc') {
+    sortedProducts.sort((a, b) => b.price - a.price);
+  }
+
+  const hasMoreProducts = sortedProducts.length < productsData.length;
+  const isLessThanTwenty = sortedProducts.length < 20;
 
   const handleAddToCart = () => {
     increaseCartCount();
@@ -30,17 +45,14 @@ const ProductGrid = ({ selectedCategory, updateCartCount }) => {
     setNumProductsToShow((prevNum) => prevNum + numProductsToLoad);
   };
 
-  const hasMoreProducts = filteredProducts.length < productsData.length;
-  const isLessThanTwenty = filteredProducts.length < 20;
-
   return (
     <Container id="product-container">
       {/* Product Counter */}
       <Box id="product-counter" mb={2}>
-        {filteredProducts.length} out of {productsData.length} products
+        {sortedProducts.length} out of {productsData.length} products
       </Box>
       <Grid container spacing={4}>
-        {filteredProducts.map((product) => (
+        {sortedProducts.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
             <ProductCard product={product} updateCartCount={handleAddToCart} />
           </Grid>
@@ -67,4 +79,9 @@ const ProductGrid = ({ selectedCategory, updateCartCount }) => {
   );
 };
 
+ProductGrid.propTypes = {
+  selectedCategory: PropTypes.string, // Replace with appropriate prop type
+  updateCartCount: PropTypes.func.isRequired,
+  sortOption: PropTypes.string, // Replace with appropriate prop type
+};
 export default ProductGrid;
